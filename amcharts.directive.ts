474189170e2a7b0369b54dfc7440b9d33bde747a
Pauan <pcxunlimited@gmail.com>
2016-10-22 16:21:02 -1000

    Fixing a bug with the listeners

--- amcharts.directive.ts
+++ amcharts.directive.ts
@@ -4,10 +4,175 @@ import { Directive, ElementRef, Input, SimpleChange } from "@angular/core";
 // TODO move this into a separate file ?
 declare var AmCharts: any;
 
-// TODO more efficient copying ?
-// TODO better type ?
-const copy = (x: any) =>
-  JSON.parse(JSON.stringify(x));
+
+function getType(x) {
+  // TODO make this faster ?
+  return {}.toString.call(x);
+}
+
+
+function copyObject(x) {
+  var output = {};
+
+  // TODO use Object.keys ?
+  for (var key in x) {
+    output[key] = copy(x[key]);
+  }
+
+  return output;
+}
+
+function copyArray(x) {
+  var length = x.length;
+
+  var output = new Array(length);
+
+  for (var i = 0; i < length; ++i) {
+    output[i] = copy(x[i]);
+  }
+
+  return output;
+}
+
+// TODO can this be made faster ?
+// TODO what about regexps, etc. ?
+function copy(x) {
+  switch (getType(x)) {
+  case "[object Array]":
+    return copyArray(x);
+
+  case "[object Object]":
+    return copyObject(x);
+
+  // TODO is this necessary ?
+  case "[object Date]":
+    return new Date(x.getTime());
+
+  default:
+    return x;
+  }
+}
+
+
+function isEqualArray(x, y) {
+  var xLength = x.length;
+  var yLength = y.length;
+
+  if (xLength === yLength) {
+    for (var i = 0; i < xLength; ++i) {
+      if (!isEqual(x[i], y[i])) {
+        return false;
+      }
+    }
+
+    return true;
+
+  } else {
+    return false;
+  }
+}
+
+function isEqualObject(x, y) {
+  // TODO use Object.keys ?
+  for (var key in x) {
+    if (key in y) {
+      if (!isEqual(x[key], y[key])) {
+        return false;
+      }
+
+    } else {
+      return false;
+    }
+  }
+
+  // TODO use Object.keys ?
+  for (var key in y) {
+    if (!(key in x)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isNaN(x) {
+  return x !== x
+}
+
+function isEqual(x, y) {
+  var xType = getType(x);
+  var yType = getType(y);
+
+  if (xType === yType) {
+    switch (xType) {
+    case "[object Array]":
+      return isEqualArray(x, y);
+
+    case "[object Object]":
+      return isEqualObject(x, y);
+
+    case "[object Date]":
+      return x.getTime() === y.getTime();
+
+    case "[object Number]":
+      return x === y || (isNaN(x) && isNaN(y))
+
+    default:
+      return x === y;
+    }
+
+  } else {
+    return false;
+  }
+}
+
+
+function removeChartListeners(chart, listeners) {
+  if (listeners != null) {
+    // TODO use Object.keys ?
+    for (var key in listeners) {
+      var listener = listeners[key];
+
+      chart.removeListener(chart, listener.event, listener.method);
+    }
+  }
+}
+
+// TODO make this faster ?
+// TODO does this work for listeners, etc. ?
+function updateChartObject(chart, oldObj, newObj) {
+  var didUpdate = false;
+
+  // TODO use Object.keys ?
+  for (var key in newObj) {
+    // TODO make this faster ?
+    if (!(key in oldObj) || !isEqual(oldObj[key], newObj[key])) {
+      if (key === "listeners") {
+        // TODO make this faster ?
+        removeChartListeners(chart, oldObj[key]);
+      }
+
+      // TODO make this faster ?
+      chart[key] = copy(newObj[key]);
+      didUpdate = true;
+    }
+  }
+
+  // TODO use Object.keys ?
+  for (var key in oldObj) {
+    if (!(key in newObj)) {
+      if (key === "listeners") {
+        removeChartListeners(chart, oldObj[key]);
+      }
+
+      delete chart[key];
+      didUpdate = true;
+    }
+  }
+
+  return didUpdate;
+}
+
 
 @Directive({
   selector: "amCharts"
@@ -29,12 +194,12 @@ export class AmChartsDirective {
     if (x.options) {
       // Update the chart after init
       if (this.chart) {
-        // TODO make this more efficient ?
-        for (let key in this.options) {
-          this.chart[key] = copy(this.options[key]);
-        }
+        var didUpdate = updateChartObject(this.chart, x.options.previousValue, x.options.currentValue);
 
-        this.chart.validateNow(true, false);
+        if (didUpdate) {
+          // TODO is this correct ?
+          this.chart.validateNow(true, false);
+        }
       }
     }
   }
@@ -45,11 +210,12 @@ export class AmChartsDirective {
     this.el.id = this.id;
     // TODO a bit hacky
     this.el.style.display = "block";
-    // TODO more efficient copying of an object ?
     this.chart = AmCharts.makeChart(this.id, copy(this.options));
   }
 
   ngOnDestroy() {
-    this.chart.clear();
+    if (this.chart) {
+      this.chart.clear();
+    }
   }
 }
