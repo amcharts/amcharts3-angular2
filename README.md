9d53806b515c631d8da583d1b89f9698ba7f1c2d
Pauan <pcxunlimited@gmail.com>
2017-02-15 10:54:13 -1000

    Adding in index.ts and *.js files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-An official directive for amCharts V3 products for Angular 2
+An official Angular 2 plugin for amCharts V3
 
 Installation
 ============
@@ -15,39 +15,36 @@ How to use
 ```
 <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
 <script src="https://www.amcharts.com/lib/3/serial.js"></script>
+<script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
 ```
 
 ----
 
-2) In your Angular2 module, import the amCharts directive and place it into the `declarations`:
+2) In your app module, import the `AmChartsModule` module and add it to the `imports`:
 
 ```
-import { AmChartsDirective } from "amcharts3-angular2/amcharts.directive";
+import { AmChartsModule } from "amcharts3-angular2";
 
 @NgModule({
-  declarations: [ AmChartsDirective ]
+  imports: [
+    AmChartsModule
+  ]
 })
+export class AppModule {}
 ```
 
 ----
 
-3) Use the `<amCharts>` tag in your `template`:
+3) Use the `<amCharts>` tag in your `template`, and also specify the `id` and `options`:
 
 ```
 @Component({
-  template: `<amCharts [id]="id" [options]="chart" [style.width.%]="100" [style.height.%]="100"></amCharts>`
+  template: `<amCharts [id]="id" [options]="options" [style.width.%]="100" [style.height.px]="500"></amCharts>`
 })
-```
-
-----
-
-4) Specify the chart config in your component:
-
-```
 export class AppComponent {
   id = "chartdiv";
 
-  chart = {
+  options = {
     "type": "serial",
     "theme": "light",
     "dataProvider": []
@@ -58,34 +55,52 @@ export class AppComponent {
 
 ----
 
-5) If you want to dynamically change the chart config after the chart has been created, you first need to create a *copy* of the existing config:
+4) If you want to dynamically change the chart config after the chart has been created, you first need to create a *copy* of the existing config:
 
 ```
-// Make a copy of the existing config
-this.chart = JSON.parse(JSON.serialize(this.chart));
+export class AppComponent {
+  changeChart() {
+    // Make a copy of the existing config
+    this.options = JSON.parse(JSON.serialize(this.options));
 
-// Change the config
-this.chart.dataProvider = [...];
-```
-
-Alternatively, you can use a function to create a new config:
-
-```
-function makeConfig(dataProvider) {
-  return {
-    "type": "serial",
-    "theme": "light",
-    "dataProvider": dataProvider
-    ...
-  };
+    // Change the config
+    this.options.dataProvider = [...];
+  }
 }
 ```
 
+Alternatively, you can use a function or method to create a new config:
+
 ```
-// Change the config
-this.chart = makeConfig([...]);
+export class AppComponent {
+  makeConfig(dataProvider) {
+    return {
+      "type": "serial",
+      "theme": "light",
+      "dataProvider": dataProvider
+      ...
+    };
+  }
+
+  changeChart() {
+    this.options = this.makeConfig([...]);
+  }
+}
 ```
 
-Why do you need to make a copy of the config? Even if you change the properties of the object, the object itself has not changed, and therefore Angular2 does **not** update the AmCharts directive.
+Why do you need to make a copy of the config? Even if you change the properties of the object, the object itself has not changed, and therefore Angular2 does **not** update AmCharts.
 
-But if you make a copy of the object, then Angular2 realizes that the object is different, and so it updates the AmCharts directive. This is an issue with Angular2, and there isn't much we can do about it.
+But if you make a copy of the object, then Angular2 realizes that the object is different, and so it updates AmCharts. This is an issue with Angular2, and there isn't much we can do about it.
+
+----
+
+You can see some examples in the `examples` directory.
+
+## Changelog
+
+### 1.1.0
+* Various fixes
+* Adding examples
+
+### 1.0.0
+* Initial release
---
+++ amcharts.directive.js
@@ -0,0 +1,252 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+(function (dependencies, factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(dependencies, factory);
+    }
+})(["require", "exports", "@angular/core"], function (require, exports) {
+    "use strict";
+    var core_1 = require("@angular/core");
+    function getType(x) {
+        return {}.toString.call(x);
+    }
+    function hasOwnKey(obj, key) {
+        return {}.hasOwnProperty.call(obj, key);
+    }
+    function copyObject(x) {
+        var output = {};
+        for (var key in x) {
+            if (hasOwnKey(x, key)) {
+                output[key] = copy(x[key]);
+            }
+        }
+        return output;
+    }
+    function copyArray(x) {
+        var length = x.length;
+        var output = new Array(length);
+        for (var i = 0; i < length; ++i) {
+            output[i] = copy(x[i]);
+        }
+        return output;
+    }
+    function copy(x) {
+        switch (getType(x)) {
+            case "[object Array]":
+                return copyArray(x);
+            case "[object Object]":
+                return copyObject(x);
+            case "[object Date]":
+                return new Date(x.getTime());
+            default:
+                return x;
+        }
+    }
+    function isNaN(x) {
+        return x !== x;
+    }
+    function isNumberEqual(x, y) {
+        return x === y || (isNaN(x) && isNaN(y));
+    }
+    function removeChartListeners(chart, x, y) {
+        if (x !== y) {
+            if (x == null) {
+                x = [];
+            }
+            if (y == null) {
+                y = [];
+            }
+            var xLength = x.length;
+            var yLength = y.length;
+            for (var i = 0; i < xLength; ++i) {
+                var xValue = x[i];
+                var has = false;
+                for (var j = 0; j < yLength; ++j) {
+                    var yValue = y[j];
+                    if (xValue.event === yValue.event &&
+                        xValue.method === yValue.method) {
+                        has = true;
+                        break;
+                    }
+                }
+                if (!has) {
+                    chart.removeListener(chart, xValue.event, xValue.method);
+                }
+            }
+        }
+    }
+    function updateArray(a, x, y) {
+        var didUpdate = false;
+        if (x !== y) {
+            var xLength = x.length;
+            var yLength = y.length;
+            if (xLength !== yLength) {
+                a.length = yLength;
+                didUpdate = true;
+            }
+            for (var i = 0; i < yLength; ++i) {
+                if (i < xLength) {
+                    if (update(a, i, x[i], y[i])) {
+                        didUpdate = true;
+                    }
+                }
+                else {
+                    a[i] = copy(y[i]);
+                    didUpdate = true;
+                }
+            }
+        }
+        return didUpdate;
+    }
+    function update(obj, key, x, y) {
+        var didUpdate = false;
+        if (x !== y) {
+            var xType = getType(x);
+            var yType = getType(y);
+            if (xType === yType) {
+                switch (xType) {
+                    case "[object Array]":
+                        if (updateArray(obj[key], x, y)) {
+                            didUpdate = true;
+                        }
+                        break;
+                    case "[object Object]":
+                        if (updateObject(obj[key], x, y)) {
+                            didUpdate = true;
+                        }
+                        break;
+                    case "[object Date]":
+                        if (x.getTime() !== y.getTime()) {
+                            obj[key] = copy(y);
+                            didUpdate = true;
+                        }
+                        break;
+                    case "[object Number]":
+                        if (!isNumberEqual(x, y)) {
+                            obj[key] = copy(y);
+                            didUpdate = true;
+                        }
+                        break;
+                    default:
+                        if (x !== y) {
+                            obj[key] = copy(y);
+                            didUpdate = true;
+                        }
+                        break;
+                }
+            }
+            else {
+                obj[key] = copy(y);
+                didUpdate = true;
+            }
+        }
+        return didUpdate;
+    }
+    function updateObject(chart, oldObj, newObj) {
+        var didUpdate = false;
+        if (oldObj !== newObj) {
+            for (var key in newObj) {
+                if (hasOwnKey(newObj, key)) {
+                    if (hasOwnKey(oldObj, key)) {
+                        if (key === "listeners") {
+                            removeChartListeners(chart, oldObj[key], newObj[key]);
+                        }
+                        if (update(chart, key, oldObj[key], newObj[key])) {
+                            didUpdate = true;
+                        }
+                    }
+                    else {
+                        chart[key] = copy(newObj[key]);
+                        didUpdate = true;
+                    }
+                }
+            }
+            for (var key in oldObj) {
+                if (hasOwnKey(oldObj, key) && !hasOwnKey(newObj, key)) {
+                    if (key === "listeners") {
+                        removeChartListeners(chart, oldObj[key], []);
+                    }
+                    delete chart[key];
+                    didUpdate = true;
+                }
+            }
+        }
+        return didUpdate;
+    }
+    var AmChartsDirective = (function () {
+        function AmChartsDirective(el, _zone) {
+            this._zone = _zone;
+            this.el = el.nativeElement;
+        }
+        AmChartsDirective.prototype.ngOnChanges = function (x) {
+            if (x.options) {
+                if (this.chart) {
+                    var didUpdate = updateObject(this.chart, x.options.previousValue, x.options.currentValue);
+                    if (didUpdate) {
+                        this.chart.validateNow();
+                        this.chart.validateData();
+                    }
+                }
+            }
+        };
+        AmChartsDirective.prototype.ngOnInit = function () {
+            var _this = this;
+            this.el.id = this.id;
+            this.el.style.display = "block";
+            this._zone.runOutsideAngular(function () {
+                var props = copy(_this.options);
+                _this.chart = window.AmCharts.makeChart(_this.id, props);
+            });
+        };
+        AmChartsDirective.prototype.ngOnDestroy = function () {
+            if (this.chart) {
+                this.chart.clear();
+            }
+        };
+        return AmChartsDirective;
+    }());
+    __decorate([
+        core_1.Input(),
+        __metadata("design:type", String)
+    ], AmChartsDirective.prototype, "id", void 0);
+    __decorate([
+        core_1.Input(),
+        __metadata("design:type", Object)
+    ], AmChartsDirective.prototype, "options", void 0);
+    AmChartsDirective = __decorate([
+        core_1.Directive({
+            selector: "amCharts"
+        }),
+        __metadata("design:paramtypes", [core_1.ElementRef, core_1.NgZone])
+    ], AmChartsDirective);
+    exports.AmChartsDirective = AmChartsDirective;
+    var AmChartsModule = (function () {
+        function AmChartsModule() {
+        }
+        return AmChartsModule;
+    }());
+    AmChartsModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                AmChartsDirective
+            ],
+            exports: [
+                AmChartsDirective
+            ],
+            imports: []
+        }),
+        __metadata("design:paramtypes", [])
+    ], AmChartsModule);
+    exports.AmChartsModule = AmChartsModule;
+});
+//# sourceMappingURL=amcharts.directive.js.map
