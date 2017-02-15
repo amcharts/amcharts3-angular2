55521de777835de1d11c4f3b6e19e33ddd9975b4
Pauan <pcxunlimited@gmail.com>
2017-02-15 10:57:27 -1000

    Updating the amCharts import for the examples

--- examples/angular-cli/src/app/app.module.ts
+++ examples/angular-cli/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AmChartsModule } from "amcharts3-angular2/amcharts.directive";
+import { AmChartsModule } from "amcharts3-angular2";
 
 @NgModule({
   declarations: [
